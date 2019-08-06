4727629199d9318eaca2522658b9b9a9d258bcbf
Alexis Josephides <ajosephides@gmail.com>
2019-08-06 15:02:57 +0100

    change to check deployment to firebase

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload. 
+          Hello world!
         </p>
         <a
           className="App-link"
