870b556197d2c79100a0c5a43f09b21effc7c504
Ben Scott <bengscott2@gmail.com>
2019-08-08 13:46:37 +0100

    chore: direct api call to backend server

--- src/components/RecipesList.js
+++ src/components/RecipesList.js
@@ -2,7 +2,6 @@ import React from 'react';
 import InputForm from './InputForm'
 import Recipe from './Recipe'
 const Api_Key = process.env.React_App_Api_Key
-console.log(Api_Key)
 
 class RecipesList extends React.Component {
   constructor(props) {
@@ -24,12 +23,13 @@ class RecipesList extends React.Component {
   }
 
   onHandleSubmit(event) {
-    const ingredients = [this.state['ing1'],this.state['ing2'],this.state['ing3']].join(',+')
-    const api = 'https://api.spoonacular.com/recipes/findByIngredients?ingredients='+ ingredients +'&number=5&'
+    const ingredients = [this.state['ing1'],this.state['ing2'],this.state['ing3']].join(',')
+    const api = "https://spoon-call.herokuapp.com/?ingredients=" + ingredients
     fetch(api)
       .then(promise => {
         return promise.json();
       }).then(data => {
+        console.log(data);
         let recipes = data.map((recipe) => {
           return <Recipe key={recipe.id} recipe={recipe} />;
         });
