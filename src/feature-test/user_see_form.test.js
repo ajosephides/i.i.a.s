f8d301d029d62ce272e7065f2c5ae0d8b8367209
Ben Scott <bengscott2@gmail.com>
2019-08-09 16:29:21 +0100

    test: add full coverage tests for RecipesList and InputForm

--- src/feature-test/user_see_form.test.js
+++ src/feature-test/user_see_form.test.js
@@ -1,16 +1,15 @@
 // import '../setupTests.js'
 // import App from '../App';
-
+//
 // describe('The main app', () => {
 //   let app;
 //   beforeEach(() => {
 //       app  = mount(<App/>);
 //   })
-
+//
 //   it('the app should have text', () => {
 //     app.simulate("change", { target: { value: "foo" }})
 //     app.simulate('click');
 //     expect(app.text()).toBe('ingredient');
 //   });
 // });
-
