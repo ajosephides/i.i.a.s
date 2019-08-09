2b44b7443f86678a41148f3920916d12358f24b8
Ben Scott <bengscott2@gmail.com>
2019-08-09 11:52:06 +0100

    WIP: add RecipeExpanded component

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,13 +1,35 @@
 import React from 'react';
+import RecipeExpanded from './RecipeExpanded'
 
 class Recipe extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showRecipeExpanded: false
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(event) {
+    this.setState(
+      this.state.showRecipeExpanded: !this.state.showRecipeExpanded
+    );
+  }
+
   render() {
     const props = this.props
     return (
       <div>
-        <h1>{props.title}</h1>
+      <a href="#" onClick={handleClick}>
+        {props.title}
+      </a>
         <img alt={props.title}  src={props.image}/>
       </div>
+
+      {if ( this.state.showRecipeExpanded ) {
+        return (<div><RecipeExpanded id={props.id}/></div>)
+        }
+      }
     )
   }
 }
---
+++ src/components/RecipeExpand.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+class RecipeExpanded extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+
+    this.componentDidMount = this.componentDidMount.bind(this);
+  }
+
+  componentDidMount() {
+    const id = this.props.id
+    const api = ""
+    fetch(api)
+  }
+
+  render() {
+    return (
+
+    )
+  }
+}
+
+export default RecipeExpanded
