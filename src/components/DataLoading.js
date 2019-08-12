ebb366d14c7ce1d051671d53db3222682a45fc0b
Alexis Josephides <ajosephides@gmail.com>
2019-08-12 14:51:57 +0100

    created simple DataLoading component

---
+++ src/components/DataLoading.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class DataLoading extends React.Component {
+
+  render() {
+    return (
+      <div>
+        Please wait while we get the data from our partners.....
+      </div>
+
+    );
+  }
+}
+
+export default DataLoading
