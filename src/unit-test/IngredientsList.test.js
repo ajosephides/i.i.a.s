1d2630433b155eda7dcf7e0496ae3173ff3118b8
Luigi <l-palermo@hotmail.it>
2019-08-13 14:31:46 +0100

    test: added feature test

--- src/unit-test/IngredientsList.test.js
+++ src/unit-test/IngredientsList.test.js
@@ -1,11 +1,20 @@
 import '../setupTest.js'
 import IngredientsList from '../components/IngredientsList';
+let wrapper
+beforeEach( () => { wrapper = shallow(<IngredientsList ingredients={ ['test'] }/>) });
 
 it('renders without crashing', () => {
   shallow(<IngredientsList ingredients={ ['test'] }/>);
 });
 
-it('renders the recipe title', () => {
-  const wrapper = shallow(<IngredientsList ingredients={ ['test'] }/>);
+it('render a ul html tag', () => {
+  expect(wrapper.type()).toEqual('ul')
+});
+
+it('render a ul html tag', () => {
+  expect(wrapper.find('li')).toHaveLength(1);
+});
+
+it('renders the ingredient just inputed', () => {
   expect(wrapper.text()).toEqual('test')
 });
