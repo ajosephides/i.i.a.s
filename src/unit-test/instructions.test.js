eebdefceb65d868b22ff0d5513becf4182f2805f
Ben Scott <bengscott2@gmail.com>
2019-08-14 09:13:39 +0100

    chore: create new Instruction component with tests

---
+++ src/unit-test/instructions.test.js
@@ -0,0 +1,18 @@
+import '../setupTest.js'
+import Instructions from '../components/Instructions';
+import fetch from '../unit-test/mocks/instructionFetch';
+global.fetch = fetch;
+
+it('renders without crashing', () => {
+  shallow(<Instructions />)
+});
+
+describe('#onComponentDidMount', () => {
+  test('calls fetch once called', () => {
+    const fetchSpy = jest.spyOn(window, 'fetch');
+    const setState = jest.fn()
+    const wrapper = shallow(<Instructions id={1}/>);
+    wrapper.instance().componentDidMount()
+    expect(fetchSpy).toBeCalled();
+  })
+});
