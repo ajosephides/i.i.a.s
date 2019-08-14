1eec795f19fe6b4ffb38aefa7135dc39e11a583d
Ben Scott <bengscott2@gmail.com>
2019-08-14 09:14:00 +0100

    chore: create new Ingredient component

---
+++ src/unit-test/mocks/ingredientFetch.js
@@ -0,0 +1,9 @@
+export default function() {
+  return Promise.resolve({
+    json: () =>
+      Promise.resolve({
+        extendedIngredients: [{id: 1, original: '1 Tbsp butter'}]
+
+    })
+  });
+};
