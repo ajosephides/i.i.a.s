93fc1ac3fdbe464daf05a6ec6f8c27b597775854
Student <student@MAKERSs-MacBook-Pro-89.local>
2019-08-14 14:53:43 +0100

    pr: break to comment on pr

--- src/feature-test/user_see_ingredient_after_add.test.js
+++ src/feature-test/user_see_ingredient_after_add.test.js
@@ -1,6 +1,6 @@
 import '../setupTest.js'
 
-describe('I.I.A.S', () => {
+xdescribe('I.I.A.S', () => {
   it('User wants to see what they are about to submit', async () => {
     const browser = await puppeteer.launch({
       // This two lines below allows the dev to see the flow on the chromium page
@@ -14,7 +14,7 @@ describe('I.I.A.S', () => {
     await page.type('input[name="ing"]', 'potatoes');
     await page.click('#add-button');
     await page.waitForSelector('div[id="ing-list"]')
-    const found = await page.evaluate(() => window.find("potatoes"));
+    const found = await page.evaluate(() => window.find("x potatoes"));
     expect(found).toBe(true)
     await browser.close();
   });
