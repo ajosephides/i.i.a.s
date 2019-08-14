d5849f01eaadf370e45fc78e7fbfe5c2ebb8448a
Student <student@makerss-mbp-89.lan>
2019-08-14 22:32:45 +0100

    dev: refactor after pr comments

--- src/feature-test/user_see_ingredient_after_add.test.js
+++ src/feature-test/user_see_ingredient_after_add.test.js
@@ -2,12 +2,7 @@ import '../setupTest.js'
 
 describe('I.I.A.S', () => {
   it('User wants to see what they are about to submit', async () => {
-    const browser = await puppeteer.launch({
-      // This two lines below allows the dev to see the flow on the chromium page
-      // slowMo: 40,
-      // headless: false,
-      // devtools: true
-    });
+    const browser = await puppeteer.launch({});
     const page = await browser.newPage();
     await page.goto('http://localhost:3000/');
     await page.waitForSelector('input[name="ing"]')
@@ -16,25 +11,6 @@ describe('I.I.A.S', () => {
     await page.waitForSelector('li')
     const found = await page.evaluate(() => window.find("potatoes"));
     expect(found).toBe(true)
-    await browser.close();
-  });
-
-  it('User wants to delete an ingredient from the list', async () => {
-    const browser = await puppeteer.launch({
-      // This two lines below allows the dev to see the flow on the chromium page
-      // slowMo: 40,
-      // headless: false,
-      // devtools: true
-    });
-    const page = await browser.newPage();
-    await page.goto('http://localhost:3000/');
-    await page.waitForSelector('input[name="ing"]')
-    await page.type('input[name="ing"]', 'potatoes');
-    await page.click('#add-ingredient');
-    await page.waitForSelector('li')
-    await page.click('#delete-ingredient');
-    const found = await page.evaluate(() => window.find("potatoes"));
-    expect(found).toBe(false)
     await browser.close();
   });
 });
