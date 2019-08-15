4d34c4c1ae01df6d4a7b5bce03aed82a8b776c20
Ben Scott <bengscott2@gmail.com>
2019-08-15 15:05:17 +0100

    style: WIP, accordion style opening for single recipe view

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,49 +1,60 @@
 import React from 'react';
+import Card from 'react-bootstrap/Card'
+import Accordion from 'react-bootstrap/Accordion'
+import Button from 'react-bootstrap/Button'
 import RecipeExpanded from './RecipeExpanded'
 
 class Recipe extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      showRecipeExpanded: false
-    };
-    this.handleClick = this.handleClick.bind(this);
-    this.showRecipeExpanded = this.showRecipeExpanded.bind(this);
-  }
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     showRecipeExpanded: false
+  //   };
+  //   // this.handleClick = this.handleClick.bind(this);
+  //   // this.showRecipeExpanded = this.showRecipeExpanded.bind(this);
+  // }
 
-  handleClick(event) {
-    this.setState(prevState => ({
-        showRecipeExpanded: !prevState.showRecipeExpanded
-      })
-    );
-    event.preventDefault()
-  }
-
-  showRecipeExpanded() {
-    if (this.state.showRecipeExpanded) {
-      return <div><RecipeExpanded id={this.props.id}/></div>
-    } else {
-      return null
-    }
-  }
+  // handleClick(event) {
+  //   this.setState(prevState => ({
+  //       showRecipeExpanded: !prevState.showRecipeExpanded
+  //     })
+  //   );
+  //   event.preventDefault()
+  // }
+  //
+  // showRecipeExpanded() {
+  //   if (this.state.showRecipeExpanded) {
+  //     return <div><RecipeExpanded id={this.props.id}/></div>
+  //   } else {
+  //     return null
+  //   }
+  // }
 
   render() {
     const props = this.props
     return (
-      <div>
-        <div>
-        <a href="recipe" onClick={this.handleClick}>
-          {props.title}
-        </a>
-          <img alt={props.title}  src={props.image}/>
-        </div>
-
-        <div>
-          {this.showRecipeExpanded()}
-        </div>
-      </div>
+      <Card>
+        <Card.Header>
+          <Accordion.Toggle as={Button} variant="link" eventKey={this.props.eventIndex}>
+            {props.title}
+          </Accordion.Toggle>
+        </Card.Header>
+        <Accordion.Collapse eventKey={this.props.eventIndex}>
+          <Card.Body>
+            <RecipeExpanded id={this.props.id} />
+          </Card.Body>
+        </Accordion.Collapse>
+      </Card>
     )
   }
 }
 
 export default Recipe
+
+
+// <div>
+//   <a href="recipe" onClick={this.handleClick}>
+//     {props.title}
+//   </a>
+//     <img alt={props.title}  src={props.image}/>
+// </div>
