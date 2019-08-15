750e85cea072818641449e0bfa4fb6b0846f387c
Luigi <l-palermo@hotmail.it>
2019-08-15 15:54:14 +0100

    style: bottom nav bar

--- src/components/InputForm.js
+++ src/components/InputForm.js
@@ -1,5 +1,12 @@
 import React from 'react';
-import Form from 'react-bootstrap/Form'
+// import Icon from "@material-ui/core/Icon";
+import AddCircle from "@material-ui/icons/AddCircle";
+import Search from "@material-ui/icons/Search";
+import Local_Dining from "@material-ui/icons/LocalDining";
+import InputGroup from 'react-bootstrap/InputGroup'
+import Button from 'react-bootstrap/Button'
+import FormControl from 'react-bootstrap/FormControl'
+import StyleForm from '../style-sheet/StyleForm.css'
 
 class InputForm extends React.Component {
   constructor(props) {
@@ -24,26 +31,70 @@ class InputForm extends React.Component {
   }
 
   render() {
-    const formStyle = {
-      padding: '1%',
-      backgroundColor: 'rgba(59, 59, 59, 0.692)',
-      fontFamily: "-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif",
-    };
-    const inputStyle = {
-      backgroundColor: 'transparent',
+    const transp = {
+      // backgroundColor: 'transparent',
+      // border: 'none',
+      // color: 'white',
+      // fontSize: '120%',
+      // width: 'fit-content'
     }
+
+    const input = {
+      // backgroundColor: 'lightgray',
+      // border: 'none',
+      // color: 'white'
+    }
+
+    const icons = {
+      // width: '89%'
+      // backgroundColor: 'lightgray',
+      // border: 'none',
+      // fontSize: '200%'
+    }
+
     return (
-      <div className="fixed-bottom" >
-        <Form inline style={formStyle}  className="justify-content-center">
+      <div>
+        {/* <form style={StyleForm} className="fixed-bottom"> */}
           <label>
-            <input className={inputStyle} type="text" name = "ing" value={this.props.ing} placeholder='ingredients' onChange={this.handleChange} />
-            <button id='add-button' onClick={this.handleChangeAdd}>
-                Add
+          <InputGroup className="mb-0 fixed-bottom" style={{}}>
+            <InputGroup.Prepend>
+              <InputGroup.Text id="basic-addon1" style={{backgroundColor: 'lightgrey', border: 'none', margin: '0%'}}>
+                <Local_Dining/>
+              </InputGroup.Text>
+            </InputGroup.Prepend>
+            {/* <InputGroup.Text id="basic-addon1"> */}
+            <FormControl style={{backgroundColor: 'lightgrey'}}
+              placeholder="Username"
+              aria-label="Username"
+              aria-describedby="basic-addon1"
+              type="text" name="ing" value={this.props.ing} 
+              placeholder='Add ingredient....' 
+              onChange={this.handleChange} 
+            />
+            <InputGroup.Append>
+                <Button id='add-ingredient' style={{backgroundColor: 'lightgrey', border: 'none', margin: '0%'}} onClick={this.handleChangeAdd}>
+                  <AddCircle/>
+                </Button>
+            </InputGroup.Append>
+            <InputGroup.Append>
+                <Button type="submit" value="Submit" style={{backgroundColor: 'lightgrey', border: 'none', margin: '0%'}} onClick={this.handleSubmit}>
+                <Search/>
+                </Button>
+            </InputGroup.Append>
+          </InputGroup>
+
+
+
+            {/* <input style={input} type="text" name="ing" value={this.props.ing} placeholder='Add ingredient....' onChange={this.handleChange} /> */}
+            {/* <button style={transp} id='add-button' onClick={this.handleChangeAdd}>
+              <AddCircle open={true} style={icons}/>
             </button>
           </label>
-          <br />
-          <input type="submit" value="Submit" onClick={this.handleSubmit}/>
-        </Form>
+            <button style={transp} type="submit" value="Submit" onClick={this.handleSubmit}>
+              <Search open={true} style={icons}/>
+            </button> */}
+              </label>
+        {/* </form> */}
       </div>
     );
   }
