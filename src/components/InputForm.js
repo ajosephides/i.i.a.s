2b5f14951d2f835370c90651289d558036d7da9d
Luigi <l-palermo@hotmail.it>
2019-08-27 11:19:54 +0100

    commit to switch to branch input style

--- src/components/InputForm.js
+++ src/components/InputForm.js
@@ -39,7 +39,7 @@ class InputForm extends React.Component {
               </InputGroup.Text>
             </InputGroup.Prepend>
             <FormControl className='formControl'
-              
+            
               type="text" name="ing" value={this.props.ing} 
               placeholder='Add ingredient....' 
               onChange={this.handleChange} />
